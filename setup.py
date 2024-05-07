5106dd77a62f907d719241eed9ac9406cf82afb9
duncanchiu409 <chiuduncan409@gmail.com>
2024-05-07 15:06:45 +0800

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='myMLlib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My first Python ML library',
+    author='duncanchiu409',
+)
